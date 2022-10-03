b9b0b188366a12a934332ba8697a89fc4dd98c74
aimanali-1003 <aiman.ali@devsinc.com>
2022-10-03 12:06:27 +0500

    defining states

---
+++ src/components/Burger.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Burger extends Component {
+    state = {
+        lettuce: 0,
+        tomato: 0,
+        cheese: 0,
+        meat: 0
+    }
+
+}
