677871678b159ca569140bee23516674afc2d9a8
aimanali-1003 <aiman.ali@devsinc.com>
2022-10-03 12:07:42 +0500

    adding and removing ingredients

--- src/components/Burger.js
+++ src/components/Burger.js
@@ -7,5 +7,43 @@ export default class Burger extends Component {
         cheese: 0,
         meat: 0
     }
+    addRemoveIngredient = (action, ingredient) => {
+        let {
+            lettuce,
+            tomato,
+            cheese,
+            meat
+        } = this.state;
+
+        let stateValue;
+        switch(ingredient){
+            case 'lettuce':{
+                stateValue = lettuce;
+                break;
+            }
+            case 'tomato':{
+                stateValue = tomato;
+                break;
+            }
+            case 'cheese':{
+                stateValue = cheese;
+                break;
+            }
+            case 'meat':{
+                stateValue = meat;
+                break;
+            }
+            default: break;
+        }
+        if(action === 'add'){
+            stateValue = stateValue + 1;
+        }else{
+            stateValue = stateValue - 1;
+        }
+        this.setState({
+            [ingredient]: stateValue >= 0 ? stateValue : 0
+        });
+    }
+
 
 }
